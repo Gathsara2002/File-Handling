ab5aa2e908e9836f6d28595d331bb73babefabfd
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-17 21:13:39 +0530

    Bug fixed

--- Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,7 +1,9 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -13,4 +15,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = {"lk.ijse.spring.controller", "lk.ijse.spring.advisor"})
 public class WebAppConfig {
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(900000000);
+        return multipartResolver;
+    }
 }
